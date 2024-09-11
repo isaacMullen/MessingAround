13f793d73bce75a08ecdefb2f9af4dd83ea946c3
isaacMullen <43121643+isaacMullen@users.noreply.github.com>
2024-09-11 15:33:22 -0300

    Add files via upload

---
+++ MethodPractice/MethodPractice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice
+{
+    internal class Program
+    {                
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            
+            IEnumerator enumerator = numbers.GetEnumerator();
+
+
+            while(enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+                if(!enumerator.MoveNext())
+                {
+                    Console.WriteLine("DONE");
+                }
+            }
+            Console.ReadKey();
+        }                               
+            
+        
+    }
+}
+
